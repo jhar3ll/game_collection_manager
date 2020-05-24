b3a286dc546b823d03e13fa3dbd75b66ea011af5
jhar3ll <jharell.chambers@selu.edu>
2020-05-24 01:25:22 -0500

    eod 524

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,8 +21,7 @@ class UsersController < ApplicationController
     def show
         @user = User.find_by_id(params[:id])
         redirect_to '/' if !@user
-        @games = Game.all
-
+        @ratings = current_user.ratings
     end 
 
   
