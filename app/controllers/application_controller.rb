653c9552c82ec91d36e5bdc053dcff72469d748f
jhar3ll <jharell.chambers@selu.edu>
2020-05-28 13:18:46 -0500

    fixed if_not_logged_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,5 +13,6 @@ class ApplicationController < ActionController::Base
   
     def redirect_if_not_logged_in
       redirect_to login_path if !logged_in?
+      flash[:error] = "You must be logged in to do that!"
     end
   end
